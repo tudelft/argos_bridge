a0045ca837bb2907632b4609cc6e7a01f0aafa5d
knmcguire <k.n.mcguire@tudelft.nl>
2017-11-08 15:39:40 +0000

    [services] slimmed down service node

--- plugin/simulation/argos_ros_start_simulation.cpp
+++ plugin/simulation/argos_ros_start_simulation.cpp
@@ -9,8 +9,6 @@
 
 #include "ros/ros.h"
 #include "argos_bridge/argos_ros_start_sim.h"
-#include "argos_bridge/argos_ros_stop_sim.h"
-#include "argos_bridge/argos_ros_reset_sim.h"
 
 #include <argos3/core/simulator/simulator.h>
 #include <argos3/core/utility/configuration/argos_configuration.h>
@@ -21,48 +19,11 @@
 bool start_sim(argos_bridge::argos_ros_start_sim::Request  &req,
 		argos_bridge::argos_ros_start_sim::Response &res)
 {
+	argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();
+	cSimulator.Reset();
+	cSimulator.Execute();
+	std::cout<<"ARGoS is finished"<<std::endl;
 
-
-	if(req.start_argos){
-		argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();
-		cSimulator.SetExperimentFileName("argos_worlds/bug.argos");
-		cSimulator.LoadExperiment();
-		cSimulator.Reset();
-		cSimulator.Execute();
-		res.argos_has_started = true;}
-
-
-	return true;
-}
-
-// Reset the ARGoS Simulator
-bool reset_sim(argos_bridge::argos_ros_reset_sim::Request  &req,
-		argos_bridge::argos_ros_reset_sim::Response &res)
-{
-
-	if(req.reset_argos){
-		argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();;
-		cSimulator.Reset();
-		res.argos_has_resetted = true;
-	}
-
-	return true;
-}
-
-
-// Stop the ARGoS Simulator
-bool stop_sim(argos_bridge::argos_ros_stop_sim::Request  &req,
-		argos_bridge::argos_ros_stop_sim::Response &res)
-{
-
-
-	if(req.stop_argos){
-		argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();;
-		cSimulator.Destroy();
-
-
-		res.argos_has_stopped = true;
-	}
 
 	return true;
 }
@@ -74,11 +35,14 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 
 	ros::ServiceServer service1 = n.advertiseService("argos_ros_start_sim", start_sim);
-	ros::ServiceServer service2 = n.advertiseService("argos_ros_reset_sim", reset_sim);
-	ros::ServiceServer service3 = n.advertiseService("argos_ros_stop_sim", stop_sim);
 
 	ROS_INFO("Ready start or stop the ARGoS simulator");
 
+	//TODO: Make filename part of launch file
+	argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();
+	cSimulator.SetExperimentFileName("/home/knmcguire/Documents/Software/catkin_ws/src/argos_bridge/argos_worlds/bug.argos");
+
+	cSimulator.LoadExperiment();
 
 	ros::spin();
 
