87f30e49eccb2785ac8c9c7c748b52df42b00493
jamesbut <jjbsports16@hotmail.co.uk>
2017-11-09 14:34:06 +0000

    Made separate thread for sim_start listener

--- plugin/simulation/argos_ros_start_simulation.cpp
+++ plugin/simulation/argos_ros_start_simulation.cpp
@@ -14,40 +14,55 @@
 #include <argos3/core/simulator/simulator.h>
 #include <argos3/core/utility/configuration/argos_configuration.h>
 #include <iostream>
+#include <boost/thread.hpp>
 
+bool start_sim_bool = false;
 
-// Start the ARGoS Simulator
+// Start the ARGoS Simulator via callaback
 bool start_sim(std_srvs::Empty::Request  &req,
 					std_srvs::Empty::Response &res)
 {
-	std::cout << "Sim started" << std::endl;
-	argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();
-	cSimulator.Reset();
-	std::cout<<"ARGoS is finished"<<std::endl;
 
+	start_sim_bool = true;
 
 	return true;
 }
 
+//Thread to listen for start sim service
+void startSimServiceThread() {
+
+	ros::NodeHandle n;
+	ros::ServiceServer service1 = n.advertiseService("start_sim", &start_sim);
+	ros::spin();
+
+}
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "argos_ros_start_sim");
-	ros::NodeHandle n;
 
-	ros::ServiceServer service1 = n.advertiseService("argos_ros_start_sim", start_sim);
-
-	ROS_INFO("Ready start or stop the ARGoS simulator");
+	//Start listening for start_sim service
+	boost::thread spin_thread(&startSimServiceThread);
 
 	//TODO: Make filename part of launch file
 	argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();
-	//cSimulator.SetExperimentFileName("/home/james/catkin_ws/src/argos_bridge/argos_worlds/bug.argos");
-	cSimulator.SetExperimentFileName("/home/knmcguire/Documents/Software/catkin_ws/src/argos_bridge/argos_worlds/bug.argos");
+	cSimulator.SetExperimentFileName("/home/james/catkin_ws/src/argos_bridge/argos_worlds/bug.argos");
+	//cSimulator.SetExperimentFileName("/home/knmcguire/Documents/Software/catkin_ws/src/argos_bridge/argos_worlds/bug.argos");
 
 	cSimulator.LoadExperiment();
-	cSimulator.Reset();
-	cSimulator.Execute();
-	ros::spin();
+
+	while(true) {
+
+		//Only execute when start_sim is received from service
+		while(!start_sim_bool) {}
+
+		cSimulator.Reset();
+		cSimulator.Execute();
+
+		start_sim_bool = false;
+
+	}
+
 
 	return 0;
 }
